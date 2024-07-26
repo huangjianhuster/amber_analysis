9a236cb23e08bee0e50b416a805a4f0b6b40fb75
jianhuang <55832564+huangjianhuster@users.noreply.github.com>
2024-07-26 15:50:44 -0400

    Create run_windows_extra.sh

---
+++ channel/ubs/windows/run_windows_extra.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+module load amber/amber22
+module load openmpi/4.1.5
+
+export prmtop=final.prmtop
+
+MAX_THREAD=6
+counter=1
+
+# for i in $(seq 0.0 2.0 6.0)
+for i in 2.0 3.0 4.0;
+do
+    cuda_num=$(((${i%.*}-1)%6))
+    export CUDA_VISIBLE_DEVICES="${cuda_num}"
+    
+    # mkdir dist_${i}
+    cd dist_${i}
+
+    # cp ../com_res.rst .
+    # ln -s ../frame_*_${i}.rst ./frame_${i}.rst
+    sed -i "s/DIST/${i}/g" com_res_extra.rst
+
+    # echo "pmemd.cuda -O -i ../ubs_prod.in -o window_${i}.out -p ../$prmtop -c frame_${i}.rst -r window_${i}.rst -x window_${i}.nc -inf window_${i}.mdinfo -ref ../100ns.rst &> junk &"
+    pmemd.cuda -O -i ../ubs_prod_extra.in -o window_${i}_extra.out -p ../$prmtop -c frame_${i}.rst -r window_${i}_extra.rst -x window_${i}_extra.nc -inf window_${i}_extra.mdinfo -ref ../100ns.rst &> junk &
+
+    core=$(echo "$counter % $MAX_THREAD" | bc)
+
+    cd ..
+    
+    if [ $core -eq 0 ];then
+        wait
+    fi
+    counter=$(expr $counter + 1)
+done
